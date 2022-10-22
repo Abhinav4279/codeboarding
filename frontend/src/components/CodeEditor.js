8687d63c25807fdb918d51a0b1c761e6faf9a48d
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-10-22 11:20:32 +0530

    setup monaco-editor

---
+++ frontend/src/components/CodeEditor.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import Editor from "@monaco-editor/react";
+
+const CodeEditor = () => {
+  const default_snip = `  #include <iostream>
+  using namespace std;
+
+  int main() {
+      cout << "Hello World";
+  }
+  `
+  return (
+    <Editor
+      height="100vh"
+      width="41%"
+      theme="vs-dark"
+      defaultLanguage="cpp"
+      defaultValue={default_snip}
+      // onChange={handleEditorChange}
+    />
+  )
+}
+
+export default CodeEditor
--- frontend/src/components/Editor.js
+++
@@ -1,7 +0,0 @@
-const Editor = () => {
-  return (
-    <div>Editor</div>
-  )
-}
-
-export default Editor
