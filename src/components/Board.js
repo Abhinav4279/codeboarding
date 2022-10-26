7537c200f8f45f5bbc510e4f7709638f8387e88e
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-10-26 12:04:41 +0530

    add toolbar

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,17 @@
 import { useRef, useState } from 'react';
-import { Stage, Layer, Line, Text, Circle } from 'react-konva';
+import { Stage, Layer, Line, Text } from 'react-konva';
 import styles from '../css/Codeboard.module.css'
 
 const Board = () => {
   const [tool, setTool] = useState('pen');
   const [lines, setLines] = useState([]);
-  const [circles, setCircles] = useState([]);
   const isDrawing = useRef(false);
-
-  const getDist = (x1, y1, x2, y2) => {
-    const dx = Math.abs(x2 - x1);
-    const dy = Math.abs(y2 - y1);
-    return Math.sqrt(dx * dx + dy * dy);
-  };
 
   const handleMouseDown = (e) => {
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
 
-    if (tool === "pen" || tool === "eraser")
-      setLines([...lines, { tool, points: [pos.x, pos.y] }]);
-    if (tool === "circle")
-      setCircles([...circles, { tool, x: pos.x, y: pos.y, r: 0 }]);
+    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
   };
 
   const handleMouseMove = (e) => {
@@ -32,29 +22,22 @@ const Board = () => {
     const stage = e.target.getStage();
     const point = stage.getPointerPosition();
 
-    if (tool === "pen" || tool === "eraser") {
-      let lastLine = lines[lines.length - 1];
-      // add point
-      lastLine.points = lastLine.points.concat([point.x, point.y]);
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
 
-      // replace last
-      lines.splice(lines.length - 1, 1, lastLine);
-      setLines(lines.concat());
-    } 
-    if (tool === "circle") {
-      let lastCircle = circles[circles.length - 1];
-      // add circle
-      lastCircle.r = getDist(lastCircle.x, lastCircle.y, point.x, point.y);
-
-      // replace last
-      circles.splice(circles.length - 1, 1, lastCircle);
-      setCircles(circles.concat());
-    }
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
   };
 
   const handleMouseUp = () => {
     isDrawing.current = false;
   };
+
+  const cleanBoard = () => {
+    setLines([]);
+  }
 
   return (
     <div className={styles.board} style={{width: '41%'}}>
@@ -62,6 +45,20 @@ const Board = () => {
       <div className={styles.pin} style={{ top: '0.6rem', right: '0.6rem'}}></div>
       <div className={styles.pin} style={{ bottom: '0.6rem', left: '0.6rem'}}></div>
       <div className={styles.pin} style={{ bottom: '0.6rem', right: '0.6rem'}}></div>
+
+      <div className={styles.toolbar}>
+        <select
+          value={tool}
+          onChange={(e) => {
+            setTool(e.target.value);
+          }}
+          className={styles.tools}
+        >
+          <option value="pen">Pen</option>
+          <option value="eraser">Eraser</option>
+        </select>
+        <button className={styles.clear} onClick={() => cleanBoard()}>Clear</button>
+      </div>
 
       <Stage
         width={window.innerWidth}
@@ -76,7 +73,7 @@ const Board = () => {
               key={i}
               points={line.points}
               stroke="#FF4C29"
-              strokeWidth={5}
+              strokeWidth={line.tool === "eraser" ? 15: 5}
               tension={0.5}
               lineCap="round"
               lineJoin="round"
@@ -85,28 +82,8 @@ const Board = () => {
               }
             />
           ))}
-          {circles.map((circle, i) => (
-            <Circle
-              key={i}
-              x={circle.x}
-              y={circle.y}
-              radius={circle.r}
-              stroke="#FF4C29"
-              strokeWidth={5}
-            />
-          ))}
         </Layer>
       </Stage>
-      <select
-        value={tool}
-        onChange={(e) => {
-          setTool(e.target.value);
-        }}
-      >
-        <option value="pen">Pen</option>
-        <option value="circle">Circle</option>
-        <option value="eraser">Eraser</option>
-      </select>
     </div>
   );
 };
