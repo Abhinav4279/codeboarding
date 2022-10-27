0c221422f53b21beac7afdf7fca6035d273c36f7
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-10-27 10:52:32 +0530

    add dynamic route for roomid

--- src/pages/CodeBoard.js
+++ src/pages/CodeBoard.js
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Dashboard from '../components/Dashboard';
 import CodeEditor from '../components/CodeEditor';
 import Board from '../components/Board';
 import styles from '../css/Codeboard.module.css'
+import { initSocket } from '../socket';
+import { useLocation } from 'react-router-dom';
+import ACTIONS from '../Actions';
 
 const USERS = [
   {
@@ -20,6 +23,20 @@ const USERS = [
 ]
 
 const CodeBoard = () => {
+  // const socketRef = useRef(null);
+  // const location = useLocation();
+  // useEffect(() => {
+  //   const init = async () => {
+  //     socketRef.current = await initSocket();
+  //     socketRef.current.emit(ACTIONS.JOIN, {
+  //       roomId,
+  //       username: location.state.username,
+  //     });
+  //   }
+
+  //   init();
+  // }, []);
+
   const [users, setUsers] = useState(USERS);
   let room_code = '53245jljljqwe45';
 
