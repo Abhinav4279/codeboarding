41a25a28a365c6915636dea9b7c10a03082ce2fb
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-10-27 15:46:31 +0530

    server socket join event handle

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const {Server} = require('socket.io')
 const express = require('express');
 const http = require('http');
+const ACTIONS = require('./src/Actions');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const userSocketMap = {};
+function getAllConnectedClients(roomId) {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      }
+    }
+  )
+}
+
 io.on('connection', (socket) => {
-  console.log('connected', socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socketId,
+      })
+    })
+  })
 })
 
 const PORT = process.env.PORT || 5000;
