0f3e4d8ea7928f244e38757ec5ddd334dc76601d
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-10-27 16:07:58 +0530

    emit disconnecting socket

--- server.js
+++ server.js
@@ -32,6 +32,20 @@ io.on('connection', (socket) => {
       })
     })
   })
+
+  socket.on('disconnecting', () => {
+    const rooms = [...socket.rooms];
+
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      })
+    })
+
+    delete userSocketMap[socket.id]
+    socket.leave();
+  })
 })
 
 const PORT = process.env.PORT || 5000;
