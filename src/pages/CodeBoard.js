d032d076169ecac311cfb6744fc97b68ae8e30ed
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-10-27 15:48:24 +0530

    client socket joined event handle

--- src/pages/CodeBoard.js
+++ src/pages/CodeBoard.js
@@ -8,29 +8,15 @@ import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom'
 import ACTIONS from '../Actions';
 import toast from 'react-hot-toast';
 
-const USERS = [
-  {
-    id: 1,
-    name: "Akshay Bir"
-  },
-  {
-    id: 2,
-    name: "Naveen Narayan"
-  },
-  {
-    id: 3,
-    name: "Ravi Kumar"
-  }
-]
-
 const CodeBoard = () => {
   const socketRef = useRef(null);
   const location = useLocation();
-  const {roomId} = useParams();
+  const { roomId } = useParams();
   const navigateTo = useNavigate();
 
-  const [users, setUsers] = useState(USERS);
+  const [users, setUsers] = useState([]);
 
+  const effectRan = useRef(false)
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
@@ -47,17 +33,29 @@ const CodeBoard = () => {
         roomId,
         username: location.state?.username,
       });
+
+      //JOINED event
+      socketRef.current.on(ACTIONS.JOINED, 
+        ({ clients, username, socketId }) => {
+          if(username !== location.state?.username) {
+            toast.success(`${username} joined.`)
+          }
+          setUsers(clients)
+      })
     }
 
-    init();
+    if(effectRan.current === false)
+      init();
+
+    return () => effectRan.current = true
   }, []);
 
-  if(!location.state)
+  if (!location.state)
     return <Navigate to='/' />
 
   return (
     <div className={styles.main_wrap}>
-      <Dashboard users={users} room_code={roomId}/>
+      <Dashboard users={users} room_code={roomId} />
       <CodeEditor />
       <Board />
     </div>
